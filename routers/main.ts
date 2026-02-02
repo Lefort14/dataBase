c66a8eab5372afb93dc7a3194ccc096f94f93fe4
Lefort14 <maxibon2004@gmail.com>
2026-02-02 14:32:48 +0300

    new .gitignore

--- routers/main.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import adminRouter from './admin.ts'
-const router = express.Router()
- 
-router.get('/', (req, res) => res.redirect('/admin'))
-router.use('/admin', adminRouter)
-
-export default router
