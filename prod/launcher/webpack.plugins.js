61306963f31a27ccab2fc0620650d9d9a7dc5256
Lefort14 <maxibon2004@gmail.com>
2026-02-04 15:18:19 +0300

    удалил папку prod

--- prod/launcher/webpack.plugins.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-export const plugins = [
-    new ForkTsCheckerWebpackPlugin({
-        logger: 'webpack-infrastructure',
-    }),
-];
--- prod/start.js
+++
@@ -1,6 +0,0 @@
-import { startServer, DATA_PORT } from "./index.js";
-startServer(DATA_PORT);
-/*
-* Переписал зависимости из electron forge и перенёс запуск проекта в start.ts
-* Не понимаю где подключать url с ejs
-*/ 
